2489329f24668f2bea154560d7129c6abbe810af
ChadMourning <chadmourning@gmail.com>
2019-02-07 00:31:27 -0500

    Add files via upload
    The starter code for Assignment 3.

---
+++ hw_broken.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int deviation( int* a, int n);
+
+int main()
+{
+	std::vector< int > v;
+	int _10 = 10;
+	while( _10 >= 0 )
+	{
+		v.push_back(_10);
+		std::cout << deviation(v) << std::endl;
+		_10 - 1;
+	}
+}
+
+int deviation( int* a, int n )
+{
+	int sum;
+	for(size_t i = 0; i <= v.size(); i++)
+	{
+		sum += v[i];
+	} 
+	double mean = sum /= v.size();
+	double stddev = 0;
+	for(size_t i = 0; i <= v.size() -1; i++)
+	{
+		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+	}
+	stddev /= v.size();
+	if( stddev = 0)
+		std::cout << "Sigma is zero." << std::endl;
+	return sqrt(stddev);
+}
